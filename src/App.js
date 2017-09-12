104bb7094add598bd54815f9f289dfc5a9a98ca7
Ben Kelly <Benjamin.J.Kelly@live.com>
2017-09-11 22:15:22 -0400

    feat: Display generic comments

--- src/App.js
+++ src/App.js
@@ -2,56 +2,63 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {
-  Media
+  Media,
+  ButtonToolbar,
+  Button
 } from 'reactstrap';
 
 class App extends Component {
+  
   render() {
     return (
       <div className="App">
 
-    <Media.List>
-      <Media.ListItem>
-        <Media.Left>
-          <img width={64} height={64} src="/assets/thumbnail.png" alt="Image"/>
-        </Media.Left>
-        <Media.Body>
-          <Media.Heading>Media heading</Media.Heading>
-          <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.</p>
+        <ButtonToolbar>
+    {/* Standard button */}
+    <Button>Default</Button>
 
-            <Media>
-              <Media.Left>
-                <img width={64} height={64} src="/assets/thumbnail.png" alt="Image"/>
-              </Media.Left>
-              <Media.Body>
-                <Media.Heading>Nested media heading</Media.Heading>
-                <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.</p>
+    {/* Provides extra visual weight and identifies the primary action in a set of buttons */}
+    <Button bsStyle="primary">Primary</Button>
 
-                <Media>
-                  <Media.Left>
-                    <img width={64} height={64} src="/assets/thumbnail.png" alt="Image"/>
-                  </Media.Left>
-                  <Media.Body>
-                    <Media.Heading>Nested media heading</Media.Heading>
-                    <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.</p>
-                  </Media.Body>
-                </Media>
-              </Media.Body>
-            </Media>
+    {/* Indicates a successful or positive action */}
+    <Button bsStyle="success">Success</Button>
 
-            <Media>
-              <Media.Left>
-                <img width={64} height={64} src="/assets/thumbnail.png" alt="Image"/>
-              </Media.Left>
-              <Media.Body>
-                <Media.Heading>Nested media heading</Media.Heading>
-                <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.</p>
-              </Media.Body>
-            </Media>
-        </Media.Body>
-      </Media.ListItem>
-    </Media.List>
+    {/* Contextual button for informational alert messages */}
+    <Button bsStyle="info">Info</Button>
 
+    {/* Indicates caution should be taken with this action */}
+    <Button bsStyle="warning">Warning</Button>
+
+    {/* Indicates a dangerous or potentially negative action */}
+    <Button bsStyle="danger">Danger</Button>
+
+    {/* Deemphasize a button by making it look like a link while maintaining button behavior */}
+    <Button bsStyle="link">Link</Button>
+  </ButtonToolbar>
+
+      <ul class="list-unstyled">
+  <li class="media">
+    <img src={require('./pyle1.jpg')} />
+    <div class="media-body">
+      <h5 class="mt-0 mb-1">List-based media object</h5>
+      Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+    </div>
+  </li>
+  <li class="media my-4">
+    <img src={require('./recall2.jpg')} />
+    <div class="media-body">
+      <h5 class="mt-0 mb-1">List-based media object</h5>
+      Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+    </div>
+  </li>
+  <li class="media">
+    <img src={require('./recall1.jpg')} />
+    <div class="media-body">
+      <h5 class="mt-0 mb-1">List-based media object</h5>
+      Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+    </div>
+  </li>
+</ul>
 
 
   </div>
